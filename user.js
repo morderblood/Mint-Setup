7c1129b5a63131f3496034054c8ecd16849b6cee
653232023 <653232023>
2026-04-28 23:06:38 +0200

    firefox config

---
+++ user.js
@@ -0,0 +1,7 @@
+user_pref("gfx.webrender.software", true);
+user_pref("browser.sessionstore.interval", 600000);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("browser.low_commit_space_threshold_percent", 100);
+user_pref("browser.tabs.min_inactive_duration_before_unload", 3600000);
+user_pref("browser.tabs.inTitlebar", 1);
