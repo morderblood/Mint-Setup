854ed062fb89b016fcd292f8431f45661b0e13d6
Vladyslav Kravchenko <102025446+morderblood@users.noreply.github.com>
2026-04-28 21:37:45 +0200

    Create wallpaper.sh

---
+++ wallpaper.sh
@@ -0,0 +1,24 @@
+# Set wallpaper
+WALL1="$(pwd)/wallpaper.jpg"
+WALL2="/usr/share/backgrounds/linuxmint-wallpapers/porioni_dark.jpg"
+
+if [ -f "$WALL1" ]; then
+  WALL="$WALL1"
+elif [ -f "$WALL2" ]; then
+  WALL="$WALL2"
+else
+  echo "No wallpaper found"
+  exit 1
+fi
+
+gsettings set org.cinnamon.desktop.background picture-uri "file://$WALL"
+gsettings set org.cinnamon.desktop.background picture-options 'zoom'
+
+sudo mkdir -p /var/lib/AccountsService/icons
+
+sudo cp user.png /var/lib/AccountsService/icons/$USER
+sudo bash -c "cat > /var/lib/AccountsService/users/$USER" <<EOF
+[User]
+Icon=/var/lib/AccountsService/icons/$USER
+EOF
+sudo systemctl restart accounts-daemon
