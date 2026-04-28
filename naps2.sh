ec7d3f24591e9339f9b246a0bb8ee23cbab2ec17
Vladyslav Kravchenko <102025446+morderblood@users.noreply.github.com>
2026-04-28 23:26:44 +0200

    Add script to install NAPS2 via Apt

---
+++ naps2.sh
@@ -0,0 +1,9 @@
+# Download the NAPS2 public key
+curl -fsSL https://www.naps2.com/naps2-public.pgp | sudo gpg --dearmor -o /etc/apt/keyrings/naps2.gpg
+
+# Add NAPS2 as an Apt source
+echo "deb [signed-by=/etc/apt/keyrings/naps2.gpg] https://downloads.naps2.com ./" | sudo tee /etc/apt/sources.list.d/naps2.list >/dev/null
+
+# Install NAPS2
+sudo apt update
+sudo apt install naps2
